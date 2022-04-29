cbd5fd2b0aea1794123ddfa0211963bb09bd2d37
admin <admin@YD4-15>
2022-04-29 15:40:22 +0900

    220429

--- P220429/src/com/yedam/java/ch08_01/Main.java
+++ P220429/src/com/yedam/java/ch08_01/Main.java
@@ -6,9 +6,16 @@ public class Main {
 
 		String str1 = new String("홍길동");
 		String str2 = new String("홍길동");
-		
-		
-		
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			if (i == 5) {
+				System.exit(0);
+				// return; main에서만 같은 역할
+				// break; for문에서 벗어남 마무리 코드 출력
+			}
+		}
+		System.out.println("마무리 코드");
 
 	}
 
