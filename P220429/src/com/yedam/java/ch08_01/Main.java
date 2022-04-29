8e0c6affc5caa023b93dd953cbe7cabcbdf7c765
admin <admin@YD4-15>
2022-04-29 12:09:42 +0900

    220429

---
+++ P220429/src/com/yedam/java/ch08_01/Main.java
@@ -0,0 +1,15 @@
+package com.yedam.java.ch08_01;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String str1 = new String("홍길동");
+		String str2 = new String("홍길동");
+		
+		
+		
+
+	}
+
+}
