07f714c2391dc2d506e616b4bbda0b4b10b0ec6a
admin <admin@YD4-15>
2022-05-06 14:38:19 +0900

    220506

--- prj220506/src/main/java/com/yedam/prj220506/App.java
+++ prj220506/src/main/java/com/yedam/prj220506/App.java
@@ -1,5 +1,6 @@
 package com.yedam.prj220506;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -17,13 +18,27 @@ public class App {
 //		st.pop();
 //		st.show();
 
-		while (true) {
-			System.out.println("============================");
-			System.out.println("로또 몇 게임을 구매하시겠습니까?");
-			int number = scan.nextInt();
-			Lotto lt = new Lotto(number);
-			lt.showLotto();
-			System.out.println("행운을 빕니다~");
+//		while (true) {
+//			System.out.println("============================");
+//			System.out.println("로또 몇 게임을 구매하시겠습니까?");
+//			int number = scan.nextInt();
+//			Lotto lt = new Lotto(number);
+//			lt.showLotto();
+//			System.out.println("행운을 빕니다~");
+//		}
+
+		Baseball bb = new Baseball();
+
+		boolean again = true;
+
+		while (again) {
+			List<Integer> computer = bb.create();
+			String result = "";
+			while (!result.equals("3스트라이크")) {
+				result = bb.judgement(computer, bb.playerNumber());
+				System.out.println(result);
+			}
+			again = bb.playagain();
 		}
 
 	}
