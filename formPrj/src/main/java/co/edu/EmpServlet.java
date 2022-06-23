4c4aea9a16b707b1e565634c3dd701db2d7b8d6f
YJE <primula374@naver.com>
2022-06-23 16:34:51 +0900

    220623

--- formPrj/src/main/java/co/edu/EmpServlet.java
+++ formPrj/src/main/java/co/edu/EmpServlet.java
@@ -1,11 +1,14 @@
 package co.edu;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @WebServlet({ "/EmpServlet", "/emplist" })
 public class EmpServlet extends HttpServlet {
@@ -19,13 +22,16 @@ public class EmpServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		EmpDAO dao = new EmpDAO();
+		List<Employees> list = dao.empList();
+
+		Gson gson = new GsonBuilder().create();
+		response.getWriter().print(gson.toJson(list));
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		doGet(request, response);
 	}
 
 }
