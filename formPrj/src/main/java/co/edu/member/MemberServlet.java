22758fa9e9249f7f3619ae3d89bc53265ed81926
YJE <primula374@naver.com>
2022-06-24 14:26:02 +0900

    220624

--- formPrj/src/main/java/co/edu/member/MemberServlet.java
+++ formPrj/src/main/java/co/edu/member/MemberServlet.java
@@ -16,70 +16,62 @@ public class MemberServlet extends HttpServlet {
 
 	public MemberServlet() {
 		super();
-
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 응답 정보 한글처리
+
+		// 응답정보에 한글처리.
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("text/json;charset=utf-8");
 
 		String cmd = request.getParameter("cmd");
-		Gson gson = new GsonBuilder().create(); // JSON 데이터 생성
+		Gson gson = new GsonBuilder().create(); // json 데이터 생성.
 		MemberDAO dao = new MemberDAO();
 
 		if (cmd.equals("list")) {
-			// 전체 리스트 => JSON 화면 보여주기
+			// 전체 리스트 => json 화면 보여주기.
 			List<MemberVO> list = dao.memberList();
 			response.getWriter().print(gson.toJson(list));
 
 		} else if (cmd.equals("insert")) {
 			String name = request.getParameter("name");
 			String addr = request.getParameter("addr");
-			String phone = request.getParameter("phone");
-			String birth = request.getParameter("birth");
-			String image = request.getParameter("image");
 
 			MemberVO vo = new MemberVO();
 			vo.setMembName(name);
 			vo.setMembAddr(addr);
-			vo.setMembPhone(phone);
-			vo.setMembBirth(birth);
-			vo.setMembImage(image);
-
 			dao.insertMember(vo);
-			// {"retCode":"Success"}
 			response.getWriter().print(gson.toJson(vo));
 
 		} else if (cmd.equals("update")) {
-			// 19 - 전화번호
-
+			// 19 - 전화번호.
 			String numb = request.getParameter("no");
-			String name = request.getParameter("nm");
 			String phone = request.getParameter("ph");
-			String addr = request.getParameter("ad");
-			String birth = request.getParameter("bt");
-			String image = request.getParameter("ig");
 
 			MemberVO vo = new MemberVO();
-			vo.setMembNo(Integer.parseInt(numb));
-			vo.setMembName(name);
+			vo.setMembNo(Integer.parseInt(numb)); // "19"
 			vo.setMembPhone(phone);
-			vo.setMembAddr(addr);
-			vo.setMembBirth(birth);
-			vo.setMembImage(image);
 
 			if (dao.updateMember(vo)) {
-				// {"retCode":"Success"}
+				// {"retCode": "Success"}
 				response.getWriter().print("{\"retCode\": \"Success\"}");
 			} else {
-				// {"retCode":"Fail"}
-				response.getWriter().print("{\"retCode\":\"Fail\"}");
+				// {"retCode": "Fail"}
+				response.getWriter().print("{\"retCode\": \"Fail\"}");
+			}
+
+		} else if (cmd.equals("delete")) {
+			String delNo = request.getParameter("delNo");
+
+			if (dao.deleteMember(Integer.parseInt(delNo))) {
+				response.getWriter().print("{\"retCode\": \"Success\"}");
+			} else {
+				// {"retCode": "Fail"}
+				response.getWriter().print("{\"retCode\": \"Fail\"}");
 			}
 
 		}
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
