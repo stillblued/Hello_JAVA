025a509028e7b60f71de897a0476a29610d24700
YJE <primula374@naver.com>
2022-06-29 16:00:56 +0900

    220629

--- formPrj/src/main/java/co/edu/DAO.java
+++ formPrj/src/main/java/co/edu/DAO.java
@@ -14,7 +14,7 @@ public class DAO {
 	public void getConnect() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
