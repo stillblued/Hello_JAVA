3c3346105b50d6197233c455cff5ac7f4302e32f
YJE <primula374@naver.com>
2022-06-30 10:34:31 +0900

    220630

--- formPrj/src/main/webapp/upload_jquery.js
+++ formPrj/src/main/webapp/upload_jquery.js
@@ -20,35 +20,40 @@ $(document).ready(function() {
 
 
 	function makeTr(elem) {
+		//null 0 '' undefined false
+		let imgSrc = elem.membImage ? '<img width="50px" src="images/' + elem.membImage + '">' : '';
 		let btn = $('<button />').text('삭제');
-		btn.on('click', function() { //이벤트 비동기 호출
-			$.ajax({
-				url: 'member',
-				method: 'post',
-				data: 'cmd=remove&delNo=' + elem.membNo,
-				success: function(result) {
-					if (result.retCode == 'Success') {
-						$('#' + elem.membNo).remove();
-					} else {
-						alert('처리중 에러');
-					};
-				},
-				error: function(err) {
-					console.error(err);
-				}
-			});
-		});
+		btn.on('click', removeCallBack);
 		return $('<tr />').attr('id', elem.membNo)
 			.append($('<td />').text(elem.membNo),
 				$('<td />').text(elem.membName),
 				$('<td />').text(elem.membPhone),
 				$('<td />').text(elem.membAddr),
 				$('<td />').text(elem.membBirth),
-				$('<td />').html('<img width="50px" src="images/' + elem.membImage + '">'),
+				$('<td />').html(imgSrc),
 				$('<td />').append(btn),
 				$('<td />').append($('<input type = checkbox>'))
 			)
 	}
+
+	function removeCallBack(e) { //이벤트 비동기 호출
+		let membNo = $(e.target).parent().parent().attr('id');
+		$.ajax({
+			url: 'member',
+			method: 'post',
+			data: 'cmd=remove&delNo=' + membNo,
+			success: function(result) {
+				if (result.retCode == 'Success') {
+					$('#' + membNo).remove();
+				} else {
+					alert('처리중 에러');
+				};
+			},
+			error: function(err) {
+				console.error(err);
+			}
+		});
+	};
 
 
 
